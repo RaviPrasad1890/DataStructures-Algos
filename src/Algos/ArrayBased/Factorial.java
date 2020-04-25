1935d27944e04f6f442feb949eddcae353d8dbc9
Ravi Prasad <ravi.prasad.1890@gmail.com>
2020-04-25 19:41:48 +0530

    Recurssion

---
+++ src/Algos/ArrayBased/Factorial.java
@@ -0,0 +1,15 @@
+package Algos.ArrayBased;
+
+public class Factorial {
+
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }else{
+            return n* fact(n-1);
+        }
+    }
+}
---
+++ src/Algos/ArrayBased/Fibonacci.java
@@ -0,0 +1,17 @@
+package Algos.ArrayBased;
+
+public class Fibonacci {
+    public static void main(String[] args){
+        System.out.println(fib(8));
+    }
+     public static int fib(int n){
+        if(n<1){
+            System.out.println("Error Message");
+            return -1;
+        }else if(n==1||n==2){
+            return n-1;
+        }else{
+          return fib(n-1) +fib(n-2);
+        }
+     }
+}
